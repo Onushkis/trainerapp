f5d7eee61d914ae115696aece7edc1c63ba7bce0
Girdzijauskaitė, Ona <ona.girdijauskaite@i2cat.net>
2022-09-07 12:54:43 +0200

    added  welcomme not finish

--- src/wellcome/Wellcome.jsx
+++ src/wellcome/Wellcome.jsx
@@ -3,6 +3,8 @@ import { FaBatteryFull, FaSignal } from 'react-icons/fa'
 import { HiRss } from 'react-icons/hi'
 import { useNavigate } from 'react-router'
 import welcomebg from '../Assets/welcomebg.jpg'
+import welcomecenter from '../Assets/welcomecenter.jpg'
+
 
 const Wellcome = () => {
   const navigate= useNavigate()
@@ -17,9 +19,9 @@ const Wellcome = () => {
       </div>
     </div>
     <img src={welcomebg} alt=""  className='aspect-square rounded-lg w-full h-96 object-cover ' />
-    <img src={welcomebg} alt=""  className='aspect-square rounded-lg w-full h-96 object-cover ' />
+    <img src={welcomecenter} alt=""  className='aspect-square rounded-lg w-full h-96 object-cover ' />
     
-    <button type="button" className="block p-4 w-full text-[24px] text-center font-bold  text-[#000000] bg-[#F1C40E]  border-none focus:border-blue-500  rounded-full   dark:focus:ring-[#9771f3] dark:focus:border-[#9771f3] hover:bg-[#dddbd4] focus:outline-none focus:ring focus:ring-[#9771f3]" 
+    <button type="button" className="block p-4 w-full mt-6 mb-6 text-[24px] text-center font-bold  text-[#000000] bg-[#F1C40E]  border-none focus:border-blue-500  rounded-full   dark:focus:ring-[#9771f3] dark:focus:border-[#9771f3] hover:bg-[#dddbd4] focus:outline-none focus:ring focus:ring-[#9771f3]" 
     onClick={()=>navigate('/popularClasses')} > start training</button>
     </div>
   )
